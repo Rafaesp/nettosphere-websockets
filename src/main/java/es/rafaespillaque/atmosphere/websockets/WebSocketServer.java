27c02047609811f763fc281235979876f999bb54
Rafaesp <rafaespillaque@gmail.com>
2012-10-27 17:17:28 +0200

    IP 0.0.0.0 para que sea accesible desde cualquier otra IP. Borrado archivo de ejemplo

--- src/main/java/es/rafaespillaque/atmosphere/websockets/WebSocketServer.java
+++ src/main/java/es/rafaespillaque/atmosphere/websockets/WebSocketServer.java
@@ -6,13 +6,17 @@ import org.atmosphere.nettosphere.Config;
 import org.atmosphere.nettosphere.Nettosphere;
 
 public class WebSocketServer {
+	
+	private static final String HOST = "0.0.0.0";
+	private static final int PORT = 8081;
 
     public static void main(String[] args) throws IOException {
         Nettosphere server = new Nettosphere.Builder().config(
-                new Config.Builder().host("127.0.0.1").port(8081)
+                new Config.Builder().host(HOST).port(PORT)
                         .webSocketProtocol(WebSocketHandler.class)
                         .build()).build();
         server.start();
+        System.out.println(String.format("Servidor empezado en %s:%d", HOST, PORT));
     }   
 }
 
